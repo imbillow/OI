3c8ba242734d33fc3bfc1e88968f05b16c161e0d
billow <yaagnto@qq.com>
2019-03-21 20:47:08 +0800

    p3952:give it up?

--- luogu/P3952.cpp
+++ luogu/P3952.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    int l, o, d, ro;
+    int l, o, d, ro, ro_tmp;
     string s;
     size_t pos, ob, oe;
     char m;
@@ -23,30 +23,39 @@ int main() {
             o = stoi(s.substr(ob, oe - ob));
         }
         getchar();
-        ro = d = 0;
+        ro_tmp = ro = d = 0;
         memset(vars, 0, 100);
         bool is = true, err = false;
         try {
             for (int j = 0, k = 0; j < l; ++j) {
                 getline(cin, s);
+//                cerr << "input:\n" << s << "\n";
                 m = s[0];
                 if (m == 'F') {
                     d++;
                     auto it = find_if(s.begin(), s.end(), [](char c) { return islower(c) && c != 'n'; });
                     if (it != s.end()) {
                         if (any_of(vars, vars + k, [=](char c) { return c == *it; })) {
+                            cerr << "var " << *it << " had exist" << "\n";
                             throw exception();
                         } else {
-                            vars[k++] = *it;
+                            vars[d] = *it;
                         }
                     }
-                    auto dp = find_if(s.begin(), s.end(), [](char c) { return isdigit(c); });
-                    auto np = find(s.begin(), s.end(), 'n');
-                    if (np != s.end()) {
-                        if (np < dp)is = false;
-                        else if (d > ro && is)ro++;
+                    auto const_pos = find_if(s.begin(), s.end(), [](char c) { return isdigit(c); });
+                    auto n_pos = find(s.begin(), s.end(), 'n');
+                    if (n_pos != s.end()) {
+                        if (n_pos < const_pos)is = false;
+                        else if (d > ro_tmp && is)ro_tmp++;
                     }
-                } else if (m == 'E')d--;
+                } else if (m == 'E') {
+                    vars[d] = 0;
+                    d--;
+                }
+                ro = max(ro, ro_tmp);
+                cerr << "O(n^" << ro << "\n";
+                cerr << "tmp O(n^" << ro_tmp << "\n";
+                if (d == 0) ro_tmp = 0;
             }
             if (d != 0) throw exception();
         } catch (exception &_) {
@@ -54,6 +63,8 @@ int main() {
             err = true;
         }
         if (!err) {
+            cerr << "finial O(n^" << ro << ")" << "\n";
+
             if (ro == o) cout << "Yes" << endl;
             else cout << "No" << endl;
         }
