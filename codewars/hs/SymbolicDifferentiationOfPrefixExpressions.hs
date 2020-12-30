f9dcf2b48d628e64a52aaaf93f3e85506fc6f743
billow <billow.fun@gmail.com>
2020-12-30 15:42:45 +0800

    unparse and little fix

--- codewars/hs/SymbolicDifferentiationOfPrefixExpressions.hs
+++ codewars/hs/SymbolicDifferentiationOfPrefixExpressions.hs
@@ -4,19 +4,20 @@ import Control.Monad
 import Data.Char
 import Text.Parsec
 import Text.Parsec.String
+import Text.Printf
 
 data AST
-  = Add AST AST
-  | Sub AST AST
-  | Mul AST AST
-  | Div AST AST
+  = Add [AST]
+  | Sub [AST]
+  | Mul [AST]
+  | Div [AST]
   | Pow AST Integer
-  | Fn String AST
+  | Fn String [AST]
   | I Integer
   | N String
   deriving (Show, Eq)
 
-type Operator = AST -> AST -> AST
+type Operator = [AST] -> AST
 
 op :: Char -> Operator
 op x =
@@ -35,9 +36,7 @@ ast =
       ( choice
           [ operator,
             pow,
-            function,
-            number,
-            var
+            function
           ]
       )
       <|> number
@@ -48,7 +47,7 @@ ast =
     operator = do
       oper <- satisfy (`elem` "+-*/")
       spaces
-      op oper <$> ast <*> ast
+      op oper <$> many1 ast
     pow = do
       char '^'
       spaces
@@ -57,9 +56,29 @@ ast =
       name <- many1 letter
       guard $ name `elem` ["sin", "cos"]
       spaces
-      Fn name <$> ast
+      Fn name <$> many ast
     number = I . read <$> many1 digit
     var = N <$> many1 letter
+
+parse' :: String -> AST
+parse' s =
+  case parse ast "" s of
+    Right ast -> ast
+    _ -> error "parse failed"
+
+unparse :: AST -> String
+unparse ast =
+  case ast of
+    Add xs -> printf "(+ %s)" (f xs)
+    Sub xs -> printf "(- %s)" (f xs)
+    Mul xs -> printf "(* %s)" (f xs)
+    Div xs -> printf "(/ %s)" (f xs)
+    Pow a n -> printf "(^ %s %d)" (unparse a) n
+    Fn name xs -> printf "(%s %s)" name (f xs)
+    I i -> show i
+    N n -> n
+  where
+    f = unwords . map unparse
 
 diff :: String -> String
 diff = undefined
